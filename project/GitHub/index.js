9b9cd9eed3a5927c55de950a73f053115ee84e77
qiyei2015 <1273482124@qq.com>
2018-06-29 23:41:10 +0800

    完成计数组件开发

--- project/GitHub/index.js
+++ project/GitHub/index.js
@@ -1,6 +1,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import Banans from './src/test/Bananas'
+import FlexBox from "./src/test/FlexBox";
 
-AppRegistry.registerComponent('GitHub', () => App);
+// AppRegistry.registerComponent('GitHub', () => App);
 // AppRegistry.registerComponent('GitHub', () => Banans);
+AppRegistry.registerComponent('GitHub', () => FlexBox);
